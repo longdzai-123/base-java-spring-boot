d3939db783e5a227ca5275202a3789b8f66c5156
longlh <lehoanglongncth@gmail.com>
2025-05-16 23:26:23 +0700

    config redis

--- src/main/java/longlh/dev/base/excel/base/excel/redis/RedisConfig.java
+++ src/main/java/longlh/dev/base/excel/base/excel/redis/RedisConfig.java
@@ -8,6 +8,8 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Slf4j
 @Configuration
@@ -15,15 +17,20 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 public class RedisConfig {
     @Value("${spring.data.redis.host}")
     private String redisHost;
+
     @Value("${spring.data.redis.port}")
     private int redisPort;
 
+    @Value("${spring.data.redis.password:}")
+    private String redisPassword;
+
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
-        log.info("Connecting redis: {}:{}", redisHost, redisPort);
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-        config.setHostName(redisHost);
-        config.setPort(redisPort);
+        log.info("Connecting to Redis at {}:{}", redisHost, redisPort);
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        if (!redisPassword.isEmpty()) {
+            config.setPassword(redisPassword);
+        }
         return new JedisConnectionFactory(config);
     }
 
@@ -31,6 +38,13 @@ public class RedisConfig {
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(jedisConnectionFactory());
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        template.afterPropertiesSet();
         return template;
     }
 }
--- src/main/java/longlh/dev/base/excel/base/excel/service/impl/ContactServiceImpl.java
+++ src/main/java/longlh/dev/base/excel/base/excel/service/impl/ContactServiceImpl.java
@@ -1,7 +1,11 @@
 package longlh.dev.base.excel.base.excel.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import longlh.dev.base.excel.base.excel.dto.ContactDto;
+import longlh.dev.base.excel.base.excel.entity.Contact;
+import longlh.dev.base.excel.base.excel.repository.ContactRepository;
 import longlh.dev.base.excel.base.excel.service.ContactService;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
@@ -9,36 +13,50 @@ import javax.sql.DataSource;
 import java.util.Objects;
 
 @Service
+@RequiredArgsConstructor
 public class ContactServiceImpl implements ContactService {
-    private final JdbcTemplate jdbcTemplate;
+    //private final JdbcTemplate jdbcTemplate;
+    private final ContactRepository contactRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
 
-    public ContactServiceImpl(DataSource dataSource) {
-        jdbcTemplate = new JdbcTemplate(dataSource);
-    }
+//    public ContactServiceImpl(DataSource dataSource) {
+//        jdbcTemplate = new JdbcTemplate(dataSource);
+//    }
 
     @Override
     public ContactDto add(ContactDto contactDto) {
-        if (Objects.nonNull(contactDto)) {
-            String sql = "insert into contact(name,email,address,telephone) values (?,?,?,?)";
-            int rowAffect = jdbcTemplate.update(sql, contactDto.getName(), contactDto.getEmail(),
-                    contactDto.getAddress(), contactDto.getTelephone());
-//            if (rowAffect > 0) {
-//                sql = "select * from contact where email = ?";
-//                return jdbcTemplate.queryForObject(sql, new RowMapper<ContactDto>() {
-//                    @Override
-//                    public ContactDto mapRow(ResultSet rs, int rowNum) throws SQLException {
-//                        ContactDto dto = ContactDto.builder()
-//                                .id(rs.getInt("id"))
-//                                .name(rs.getString("name"))
-//                                .email(rs.getString("email"))
-//                                .address(rs.getString("address"))
-//                                .telephone(rs.getString("telephone"))
-//                                .build();
-//                        return dto;
-//                    }
-//                });
-//            }
-        }
+        Contact contact = Contact.builder()
+                .address(contactDto.getAddress())
+                .email(contactDto.getEmail())
+                .name(contactDto.getName())
+                .telephone(contactDto.getTelephone())
+                .build();
+        contactRepository.save(contact);
+
+//        if (Objects.nonNull(contactDto)) {
+//            String sql = "insert into contact(name,email,address,telephone) values (?,?,?,?)";
+//            int rowAffect = jdbcTemplate.update(sql, contactDto.getName(), contactDto.getEmail(),
+//                    contactDto.getAddress(), contactDto.getTelephone());
+////            if (rowAffect > 0) {
+////                sql = "select * from contact where email = ?";
+////                return jdbcTemplate.queryForObject(sql, new RowMapper<ContactDto>() {
+////                    @Override
+////                    public ContactDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+////                        ContactDto dto = ContactDto.builder()
+////                                .id(rs.getInt("id"))
+////                                .name(rs.getString("name"))
+////                                .email(rs.getString("email"))
+////                                .address(rs.getString("address"))
+////                                .telephone(rs.getString("telephone"))
+////                                .build();
+////                        return dto;
+////                    }
+////                });
+////            }
+//        }
+
+        // lưu log vào redis
+        redisTemplate.opsForValue().set(contact.getId().toString(), contact);
         return null;
     }
 
